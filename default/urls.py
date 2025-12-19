0b6c20274d2625db2217ef765ca7d1224611911e
Jasper <11330049@dcsh.tp.edu.tw>
2025-12-19 11:58:05 +0800

    Update and Delete functions

--- default/urls.py
+++ default/urls.py
@@ -5,8 +5,14 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("", poll_list),
-    path("list", PollList.as_view(), name='poll_list'),
+    #path("", poll_list),
+    path("", PollList.as_view(), name='poll_list'),
     path("<int:pk>/", PollView.as_view(), name='poll_view'),
     path("<int:oid>/vote", PollVote.as_view(), name='poll_vote'),
+    path("create", PollCreate.as_view(), name='poll_create'),
+    path("<int:pk>/edit", PollEdit.as_view(), name='poll_edit'),
+    path("<int:pid>/add-option", OptionCreate.as_view(), name="option_create"),
+    path("<int:oid>/modify", OptionEdit.as_view(), name="option_edit"),
+    path("<int:pk>/delete", PollDelete.as_view(), name='poll_delete'),
+    path("<int:pk>/remove", OptionDelete.as_view(), name='option_delete'),
 ]
--- default/views.py
+++ default/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, RedirectView
-from django.urls import reverse
+from django.views.generic import ListView, DetailView, RedirectView, CreateView, UpdateView, DeleteView
+from django.urls import reverse, reverse_lazy
 from .models import *
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse
@@ -9,7 +9,7 @@ from django.http.response import HttpResponse
 
 def poll_list(req: HttpRequest) -> HttpResponse:
     polls = Poll.objects.all()
-    return render(req, "default/list.html", {"poll_list": polls, "msg": "Hello!"})
+    return render(req, "default/poll_list.html", {"poll_list": polls, "msg": "Hello!"})
 
 class PollList(ListView):
     model = Poll
@@ -36,3 +36,45 @@ class PollVote(RedirectView):
         option.votes += 1
         option.save()
         return reverse('poll_view', kwargs={'pk': option.poll_id})
+
+class PollCreate(CreateView):
+    model = Poll
+    fields = '__all__'  # ['topic', 'description']
+    # 成功後要去的路徑固定，直接透過 success_url 屬性指定就可以了
+    success_url = reverse_lazy('poll_list')
+
+class PollEdit(UpdateView):
+    model = Poll
+    fields = '__all__'
+
+    # 成功後要去的路徑不固定，則需定義 get_success_url() 方法來回傳
+    def get_success_url(self) -> str:
+        return reverse('poll_view', kwargs={'pk': self.object.id})
+
+class OptionCreate(CreateView):
+    model = Option
+    fields = ['title']
+
+    def form_valid(self, form) -> HttpResponse:
+        form.instance.poll_id = self.kwargs['pid']
+        return super().form_valid(form)
+    def get_success_url(self) -> str:
+        return reverse('poll_view', kwargs={'pk': self.kwargs['pid']})
+
+class OptionEdit(UpdateView):
+    model = Option
+    fields=['title']
+    pk_url_kwarg = 'oid'
+    def get_success_url(self) -> str:
+        return reverse('poll_view', kwargs={'pk': self.object.poll_id})
+    
+    # "self.object" refers to the current object
+
+class PollDelete(DeleteView):
+    model = Poll
+    success_url = reverse_lazy('poll_list')
+
+class OptionDelete(DeleteView):
+    model = Option
+    def get_success_url(self) -> str:
+        return reverse('poll_view', kwargs={'pk': self.object.poll_id})
